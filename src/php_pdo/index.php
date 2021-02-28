d924e7b514317583f4997de3cc96025de71400c1
Luas Safeno <lugnorio@gmail.com>
2021-02-28 19:31:53 -0300

    Executando instruções SQL (exec)

--- src/php_pdo/index.php
+++ src/php_pdo/index.php
@@ -10,6 +10,29 @@
          // Criar instancia do PDO com informações de acesso MySQL
         $conexao = new PDO($dsn, $usuario, $senha);
 
+        /* Executar instruções SQL (exc)
+        $query = '
+            create table tb_usuarios(
+                id int not null primary key auto_increment,
+                nome varchar(50) not null,
+                email varchar(100) not null,
+                senha varchar(32) not null
+            )
+            ';
+
+       $retorno =  $conexao->exec($query);
+       echo $retorno;
+                */
+
+       $query = '
+            delete from tb_usuarios
+       ';
+
+       $retorno = $conexao->exec($query);
+       echo $retorno;
+
+
+
     }catch(PDOException $e){
         echo 'Erro : '.$e->getCode().' <br>Mensagem : '.$e->getMessage();
         //registrar erro
