a2df261987dda23ec2b8a7cd08537247cbc0e9a9
Lucas Safeno <lugnorio@gmail.com>
2021-06-02 13:41:22 -0300

    Controllers

--- src/mvc/miniframework/App/Route.php
+++ src/mvc/miniframework/App/Route.php
@@ -4,19 +4,50 @@
 # [Route] #
 class Route{
 
+    private $routes;
+
+    public function __construct(){
+        $this->initRoutes();
+        $this->run($this->getUrl());
+    } # [/constructu()]
+
+    public function getRoutes(){
+        return $this->routes;
+    } # [/getRoutes()]
+
+    public function setRoutes(array $routes){
+        $this->routes  = $routes;
+    } # [/setRoutes()]
+
     public function initRoutes() {
         $routes['home'] = array(
             'route'         => '/',
-            'controller'    => 'indexController',
+            'controller'    => 'IndexController',
             'action'        => 'index'
         );
 
         $routes['sobre_nos'] = array(
-            'route'         => '/',
-            'controller'    => 'indexController',
+            'route'         => '/sobre_nos',
+            'controller'    => 'IndexController',
             'action'        => 'sobreNos'
         );
+
+        $this->setRoutes($routes);
     } # initRoutes()
+
+    public function run($url){
+        
+        foreach($this->getRoutes() as $key => $route  ){
+            if($url == $route['route']){
+                $class = "App\\Controllers\\".ucfirst($route['controller']);
+
+                $controller = new $class;
+
+                $action = $route['action'];
+                $controller->$action();
+            }
+        }
+    } # [/run()]
 
 
     public function getUrl() {
--- src/mvc/miniframework/public/index.php
+++ src/mvc/miniframework/public/index.php
@@ -2,7 +2,5 @@
     require_once '../vendor/autoload.php';
     
     $route = new \App\Route;
-    echo "Isso está funcionando";
-    echo "<hr>";
-    print_r($route->getUrl());
+  
 ?>
