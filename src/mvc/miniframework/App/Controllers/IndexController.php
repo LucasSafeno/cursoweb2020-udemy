6fe706c49900daecc32db76753f4f3f8594b4961
Lucas Safeno <lugnorio@gmail.com>
2021-06-06 20:12:38 -0300

    Views

--- src/mvc/miniframework/App/Controllers/IndexController.php
+++ src/mvc/miniframework/App/Controllers/IndexController.php
@@ -5,13 +5,40 @@
     # [IndexController]
     class IndexController {
 
+
+        private $view;
+
+        public function __construct(){
+
+            $this->view = new \stdClass();
+
+        } # [/construct()]
+
         public function index(){
-            echo "Chegamos ao IndexController e disparamos a action index";
+
+            $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+            $this->render('index');
+
         } # [index()]
 
         public function sobreNos(){
-            echo "Chegamos ao IndexController e disparamos a action sobreNos";
+
+            $this->view->dados = array('Notebook', 'Smartphone');
+            $this->render('sobreNos');
+
         }# [/sobreNos]
+
+
+        public function render($view){
+            $classeAtual =  get_class($this);
+
+            $classeAtual =  str_replace('App\\Controllers\\', '', $classeAtual);
+            
+            $classeAtual = strtolower(str_replace('Controller', '', $classeAtual));
+
+
+            require_once "../App/Views/".$classeAtual."/".$view.".phtml";
+        } # [/render()]
 
     } # [/IndexController]
 
