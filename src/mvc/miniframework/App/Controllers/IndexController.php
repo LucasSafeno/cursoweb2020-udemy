4ad246548e99a0ae920ebd231fd126ea26814959
Lucas Safeno <lugnorio@gmail.com>
2021-06-08 13:04:56 -0300

    Conexão com Banco de dados e Models - ERRO FETCH

--- src/mvc/miniframework/App/Controllers/IndexController.php
+++ src/mvc/miniframework/App/Controllers/IndexController.php
@@ -3,6 +3,8 @@
     namespace App\Controllers;
      
     use MF\Controller\Action;
+    use App\Connection;
+    use App\Models\Produto;
 
     # [IndexController]
     class IndexController extends Action{
@@ -10,14 +12,24 @@
 
         public function index(){
 
-            $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+           // $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+
+           // Instancia conexão
+            $conn =  Connection::getDb();
+           // Instancia Model
+            $produto  = new Produto($conn);
+
+            $produtos = $produto->getProdutos();
+
+            $this->view->dados = $produtos;
+
             $this->render('index', 'layout1');
 
         } # [index()]
 
         public function sobreNos(){
 
-            $this->view->dados = array('Notebook', 'Smartphone');
+            //$this->view->dados = array('Notebook', 'Smartphone');
             $this->render('sobreNos', 'layout2');
 
         }# [/sobreNos]
