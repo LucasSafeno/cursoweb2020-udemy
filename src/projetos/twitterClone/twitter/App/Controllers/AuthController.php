2db00f62c15ba3ec2a8010a5cabd83d03559fe90
Lucas Safeno <lugnorio@gmail.com>
2021-06-15 11:49:13 -0300

    Autenticação de Usuários

---
+++ src/projetos/twitterClone/twitter/App/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+    namespace App\Controllers;
+
+    //Recursos do Framework
+    use MF\Controller\Action;
+    use MF\Model\Container;
+
+    class AuthController extends Action{
+
+        # Autenticar
+        public function Autenticar(){
+           
+            # Cria Instancia Modelo Usuario para seta valores
+            $usuario = Container::getModel('Usuario');
+            $usuario->__set('email', $_POST['email']);
+            $usuario->__set('senha', $_POST['senha']);
+
+            # Metodo para chegar no BD se o usario Existe
+            $usuario->autenticar();
+
+            if($usuario->__get('id') != '' && $usuario->__get('nome') != ''){
+            
+                    session_start();
+
+                    $_SESSION['id'] = $usuario->__get('id');
+                    $_SESSION['nome'] = $usuario->__get('nome');
+
+                    header('Location: /timeline');
+
+            }else {
+                header('Location: /?login=erro');
+            }
+          
+        }# [/Autenticar()]
+
+        public function sair(){
+            session_start();
+
+            session_destroy();
+
+            header('Location: /');
+
+        }# [/sair()]
+
+
+    } # [/AuthController]
+
+?>
