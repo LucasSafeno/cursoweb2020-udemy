251bf9d46f222325555f263c74b851f68f788746
Lucas Safeno <lugnorio@gmail.com>
2021-06-17 01:47:53 -0300

    Pesquisando por outros usuários

--- src/projetos/twitterClone/twitter/App/Controllers/AppController.php
+++ src/projetos/twitterClone/twitter/App/Controllers/AppController.php
@@ -52,6 +52,27 @@ class AppController extends Action{
 
     } # [validaAutenticacao()]
 
+    public function quemSeguir(){
+
+        $this->validaAutenticacao();
+
+        $pesquisarPor = isset($_GET['pesquisarPor']) ? $_GET['pesquisarPor'] : '';
+      
+        $usuarios = array();
+
+        if($pesquisarPor != ''){
+            
+            $usuario = Container::getModel('Usuario');
+            $usuario->__set('nome', $pesquisarPor);
+            $usuarios = $usuario->getAll();
+        }
+
+        $this->view->usuarios = $usuarios;
+
+        $this->render('quemSeguir');
+
+    } # [/quemSeguir()]
+
 
 } # [/AppController]
 ?>
